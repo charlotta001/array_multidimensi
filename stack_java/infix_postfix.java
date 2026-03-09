4cc6038d2502424a33751623ccc966b6c8f32c9c
Hafizh Nurfitrian Syah <hafizhvtuber@gmail.com>
2026-03-09 23:45:27 +0700

    add EvaluatePostfix

--- stack_java/infix_postfix.java
+++ stack_java/infix_postfix.java
@@ -4,9 +4,21 @@ import java.util.Deque;
 import java.util.Stack;  // old version
 import java.util.Vector;
 
+
+
+
+// 1. Input merupakan sebuah ekspresi aritmatika (infix)
+// 2. Output meliputi:
+// postfix expression, 
+// step by step perhitungan (berdasarkan proses push-pop stack), dan
+// hasil akhir perhitungan
+
+
+
 public class infix_postfix {
     static Scanner input = new Scanner(System.in); 
     static Deque<String> st = new ArrayDeque<String>();
+    static Vector<Integer> resultOp = new Vector<>();
     static int top = -1;
 
     public static void main(String[] args){
@@ -31,8 +43,10 @@ public class infix_postfix {
                 String user = AritmeticUser();
                 String[] stringStream = SeparateString(user);
                 Vector<String> test = InfixToPostfix(stringStream);
+                float hasil = EvaluatePostfix(test);
+                System.out.println(hasil);
 
-                cekString(stringStream);
+                // cekString(stringStream);
                 showVectorString(test);
 
 
@@ -68,8 +82,10 @@ public class infix_postfix {
         return InputUser;
     }
 
+    // postfix
     static Vector<String> InfixToPostfix(String[] ss){
         Vector<String> postfix = new Vector<>();
+        int result= 0;
         for(int i = 0; i < ss.length; i ++){
             if(ss[i].equals("(")){
                 st.push(ss[i]);
@@ -78,13 +94,18 @@ public class infix_postfix {
                     String x = st.peek();
                     postfix.add(x);
                     st.pop();
-                } st.pop();
+                    
+                } 
+                st.pop();
+
             } else if(isOperator(ss[i])){
                 while(!st.isEmpty() && !st.peek().equals("(") && precedence(st.peek()) >= precedence(ss[i])){
                     String x = st.peek();
                     postfix.add(x);
                     st.pop();
-                } st.push(ss[i]);
+                } 
+                st.push(ss[i]);
+                
             } else{
                 postfix.add(ss[i]);
             }
@@ -99,7 +120,7 @@ public class infix_postfix {
         return postfix;
     }
 
-
+ 
     // show string
     static void showString(String user){
         System.out.println(user);
@@ -115,10 +136,12 @@ public class infix_postfix {
     }
 
 
+
+
     // boolean operator
    static boolean isOperator(String op){
-    return op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/");
-}
+    return op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/")  || op.equals("^");
+    }
 
   
 static int precedence(String op){
@@ -141,6 +164,63 @@ static int precedence(String op){
     }
 
 
+    static float EvaluatePostfix(Vector<String> value){
+        float result = 0;
+        Deque<Float> st_op = new ArrayDeque<Float>();
+        
+        for(int i =0; i < value.size(); i++){
+            if(!isOperator(value.get(i))){
+                float x = Float.parseFloat(value.get(i));
+                st_op.push(x);
+            } else if(isOperator(value.get(i))){
+                float kedua = st_op.peek();
+                st_op.pop();
+                float pertama = st_op.peek();
+                st_op.pop();
+                switch(value.get(i)){
+                    case "*":
+                        {
+                            result = pertama * kedua;
+                            st_op.push(result);
+                            break;
+                        }
+                    case "^":
+                        {
+                            float tampung = pertama;
+
+                            for(int j= 0; j < kedua - 1; j++){
+                               pertama *= tampung;
+                            }
+                            result = pertama;
+                            st_op.push(result);
+                            break;
+                        }
+                    case "/":
+                        {
+                            result = pertama / kedua;
+                            st_op.push(result);
+                            break;
+                        }
+                    case "+":
+                        {
+                            result = pertama + kedua;
+                            st_op.push(result);
+                            break;
+                        }
+                    case "-":
+                        {
+                            result = pertama - kedua;
+                            st_op.push(result);
+                            break;
+                        }
+                }
+            } 
+        }
+        result = st_op.peek();
+        return result;
+    }
+
+  
     // separate string from whitespace
     static String[] SeparateString(String ss){
         String[] result = ss.split(" ");
