b8f5a42e190229961b4b64765fe0bf1f401d211c
Hafizh Nurfitrian Syah <hafizhvtuber@gmail.com>
2026-03-10 12:06:28 +0700

    add infixtopostfix stepbystep, add evaluatepostfix without step

--- stack_java/infix_postfix.java
+++ stack_java/infix_postfix.java
@@ -43,17 +43,13 @@ public class infix_postfix {
                 String user = AritmeticUser();
                 String[] stringStream = SeparateString(user);
                 Vector<String> test = InfixToPostfix(stringStream);
-                float hasil = EvaluatePostfix(test);
-                System.out.println(hasil);
+                //float hasil = EvaluatePostfix(test);
 
-                // cekString(stringStream);
-                showVectorString(test);
-
-
+                //System.out.println(hasil);
+                //showVectorString(test);
                 // test = InfixToPostfix(stringStream);
                 
                 // showString(user);
-
                 break;
             }
             case 2: 
@@ -83,44 +79,128 @@ public class infix_postfix {
     }
 
     // postfix
-    static Vector<String> InfixToPostfix(String[] ss){
-        Vector<String> postfix = new Vector<>();
-        int result= 0;
-        for(int i = 0; i < ss.length; i ++){
-            if(ss[i].equals("(")){
-                st.push(ss[i]);
-            } else if(ss[i].equals(")")){
-                while(!st.peek().equals("(")){
-                    String x = st.peek();
-                    postfix.add(x);
-                    st.pop();
+    // static Vector<String> InfixToPostfix(String[] ss){
+    //     Vector<String> postfix = new Vector<>();
+    //     for(int i = 0; i < ss.length; i ++){
+    //         if(ss[i].equals("(")){
+    //             st.push(ss[i]);
+    //         } else if(ss[i].equals(")")){
+    //             while(!st.peek().equals("(")){
+    //                 String x = st.peek();
+    //                 postfix.add(x);
+    //                 st.pop();
                     
-                } 
-                st.pop();
+    //             } 
+    //             st.pop();
 
-            } else if(isOperator(ss[i])){
-                while(!st.isEmpty() && !st.peek().equals("(") && precedence(st.peek()) >= precedence(ss[i])){
-                    String x = st.peek();
-                    postfix.add(x);
-                    st.pop();
-                } 
-                st.push(ss[i]);
+    //         } else if(isOperator(ss[i])){
+    //             while(!st.isEmpty() && !st.peek().equals("(") && precedence(st.peek()) >= precedence(ss[i])){
+    //                 String x = st.peek();
+    //                 postfix.add(x);
+    //                 st.pop();
+    //             } 
+    //             st.push(ss[i]);
                 
-            } else{
-                postfix.add(ss[i]);
-            }
-        }
+    //         } else{
+    //             postfix.add(ss[i]);
+    //         }
+    //     }
 
-        while(!st.isEmpty()){
-            String x = st.peek();
-            postfix.add(x);
-            st.pop();
-        }
+    //     while(!st.isEmpty()){
+    //         String x = st.peek();
+    //         postfix.add(x);
+    //         st.pop();
+    //     }
         
-        return postfix;
+    //     return postfix;
+    // }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+    
+
+    // try step by step
+    // logic : 
+    // aritmatika diterima sebagai input string lalu diubah ke array string pakai .splti(regex : " ");
+    // vector postfix
+    // stack nilai untuk menampung hasil dari nilai yang sudah di olah pakai countOperator
+    // ide : stack operator untuk membuat postfix, stack nilai untuk menghitung hasil
+    static Vector<String> InfixToPostfix(String[] ss){
+    Vector<String> postfix = new Vector<>();
+    Deque<Float> st_nilai = new ArrayDeque<Float>();
+    st.clear();
+
+    // loop array string
+    for(int i = 0; i < ss.length; i++){
+        // jika ( maka push ke stack
+        if(ss[i].equals("(")){
+            System.out.println("push: " + ss[i]);
+            st.push(ss[i]);
+            showStack(st);
+            
+            // jika ) maka loop stack sampai ketemu (, pop operator, masukkan ke postfix, dan hitung hasil
+        } else if(ss[i].equals(")")){
+            System.out.println("found: )");
+            while(!st.peek().equals("(")){
+                String x = st.peek();
+                st.pop();
+                float kedua = st_nilai.peek(); st_nilai.pop();
+                float pertama = st_nilai.peek(); st_nilai.pop();
+                float hasil = countOperator(x, pertama, kedua);
+                st_nilai.push(hasil);
+                postfix.add(x);
+                System.out.println("pop: " + x);
+                showStack(st);
+            }
+            // pop (
+            st.pop();
+            System.out.println("pop: (");
+
+            // jika operator maka loop, jika prioritas dari top >= token, pop top, push ke postfix, dan hitung hasil
+        } else if(isOperator(ss[i])){
+            while(!st.isEmpty() && !st.peek().equals("(") && precedence(st.peek()) >= precedence(ss[i])){
+                String x = st.peek();
+                st.pop();
+                float kedua = st_nilai.peek(); st_nilai.pop();
+                float pertama = st_nilai.peek(); st_nilai.pop();
+                float hasil = countOperator(x, pertama, kedua);
+                st_nilai.push(hasil);
+                postfix.add(x);
+                System.out.println("pop: " + x);
+                showStack(st);
+            }
+            // push operator to stack
+            System.out.println("push: " + ss[i]);
+            st.push(ss[i]);
+            showStack(st);
+        } else {
+            // push operand to postfix dan push operand to st_nilai
+            postfix.add(ss[i]);
+            st_nilai.push(Float.parseFloat(ss[i]));
+            System.out.println("push to postfix: " + ss[i]);
+            showPostfix(postfix);
+        }
+        }
+    
+
+
+    // ketika stack masih ada opertor maka pop dan push ke 
+    while(!st.isEmpty()){
+        String x = st.peek();
+        st.pop();
+        float kedua = st_nilai.peek(); st_nilai.pop();
+        float pertama = st_nilai.peek(); st_nilai.pop();
+        float hasil = countOperator(x, pertama, kedua);
+        st_nilai.push(hasil);
+        postfix.add(x);
     }
 
- 
+
+    System.out.println("Final result for aritmetic : " + st_nilai.peek());
+    return postfix;
+}
+
+
+ /////////////////////////////////////////////////////////////////////////////////////////////
     // show string
     static void showString(String user){
         System.out.println(user);
@@ -135,7 +215,23 @@ public class infix_postfix {
         System.out.println(" ");
     }
 
+    // show postfix
+    static void showPostfix(Vector <String> pf){
+        System.out.print("postfix: ");
+        for(String val: pf){
+            System.out.print(val);
+        }
+        System.out.println(" ");
+    }
 
+    // show stack
+    static void showStack(Deque<String> st_op){
+        System.out.print("stack: ");
+        for(String val : st_op){
+            System.out.print(val + " ");
+        }
+         System.out.println(" ");
+    }
 
 
     // boolean operator
@@ -155,7 +251,7 @@ static int precedence(String op){
         return -1;
     }
 }
-    
+    // print string
     static void cekString(String[] arr){
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
@@ -163,7 +259,71 @@ static int precedence(String op){
         System.out.println(" ");
     }
 
+    // result postfix with step
+    // static float EvaluatePostfix(Vector<String> value){
+    //     float result = 0;
+    //     Deque<Float> st_op = new ArrayDeque<Float>();
+        
+    //     for(int i =0; i < value.size(); i++){
+    //         if(!isOperator(value.get(i))){
+    //             float x = Float.parseFloat(value.get(i));
+    //             System.out.println("push : " + x);
+    //             st_op.push(x);
+    //         } else if(isOperator(value.get(i))){
+    //             float kedua = st_op.peek();
+    //             st_op.pop();
+    //             float pertama = st_op.peek();
+    //             st_op.pop();
+    //             switch(value.get(i)){
+    //                 case "*":
+    //                     {
+    //                         result = pertama * kedua;
+    //                         System.out.println("operator : " + value.get(i) + "-> "+ pertama + "*" + kedua + "= " + result);
+    //                         st_op.push(result);
+    //                         break;
+    //                     }
+    //                 case "^":
+    //                     {
+    //                         float tampung = pertama;
 
+    //                         for(int j= 0; j < kedua - 1; j++){
+    //                            pertama *= tampung;
+    //                         }
+    //                         result = pertama;
+    //                         System.out.println("pop operator : " + value.get(i) + "-> "+ pertama + "^" + kedua + "= " + result);
+    //                         st_op.push(result);
+    //                         break;
+    //                     }
+    //                 case "/":
+    //                     {
+    //                         result = pertama / kedua;
+    //                         System.out.println("pop operator : " + value.get(i) + "-> "+ pertama + "/" + kedua + "= " + result);
+    //                         st_op.push(result);
+    //                         break;
+    //                     }
+    //                 case "+":
+    //                     {
+    //                         result = pertama + kedua;
+    //                         System.out.println("pop operator : " + value.get(i) + "-> "+ pertama + "+" + kedua + "= " + result);
+    //                         st_op.push(result);
+    //                         break;
+    //                     }
+    //                 case "-":
+    //                     {
+    //                         result = pertama - kedua;
+    //                         System.out.println("pop operator : " + value.get(i) + "-> "+ pertama + "-" + kedua + "= " + result);
+    //                         st_op.push(result);
+    //                         break;
+    //                     }
+    //             }
+    //         } 
+    //     }
+    //     System.out.println("result of aritmetic is : " + result);
+    //     result = st_op.peek();
+    //     return result;
+    // }
+
+    // result postfix without step
     static float EvaluatePostfix(Vector<String> value){
         float result = 0;
         Deque<Float> st_op = new ArrayDeque<Float>();
@@ -192,32 +352,86 @@ static int precedence(String op){
                                pertama *= tampung;
                             }
                             result = pertama;
+                           
                             st_op.push(result);
                             break;
                         }
                     case "/":
                         {
                             result = pertama / kedua;
+                           
                             st_op.push(result);
                             break;
                         }
                     case "+":
                         {
                             result = pertama + kedua;
+                            
                             st_op.push(result);
                             break;
                         }
                     case "-":
                         {
                             result = pertama - kedua;
+                        
                             st_op.push(result);
                             break;
                         }
                 }
             } 
         }
+        
         result = st_op.peek();
         return result;
+    }
+
+
+    static float countOperator(String op, float pertama, float kedua){
+        float result = 0;
+        switch(op){
+                case "*":
+                        {
+                        result = pertama * kedua;
+                        System.out.println("operator : " + op + "-> "+ pertama + "*" + kedua + "= " + result);
+                        return result;
+                        }
+                case "^":
+                        {
+                        float tampung = pertama;
+
+                        for(int j= 0; j < kedua - 1; j++){
+                        pertama *= tampung;
+                        }
+                        result = pertama;
+                        System.out.println("pop operator : " + op + "-> "+ pertama + "^" + kedua + "= " + result);
+                        return result;
+                        }
+                case "/":
+                        {
+                        result = pertama / kedua;
+                        System.out.println("pop operator : " + op + "-> "+ pertama + "/" + kedua + "= " + result);
+                        return result;
+                        }
+                case "+":
+                        {
+                        result = pertama + kedua;
+                        System.out.println("pop operator : " + op + "-> "+ pertama + "+" + kedua + "= " + result);
+                        return result;
+                        }
+                    case "-":
+                        {
+                        result = pertama - kedua;
+                        System.out.println("pop operator : " + op + "-> "+ pertama + "-" + kedua + "= " + result);
+                        return result;
+                        }
+                }
+        return result;
+    }
+    
+    
+
+    static float stringToFloat(String value){
+        return Float.parseFloat(value);
     }
 
   
@@ -226,4 +440,5 @@ static int precedence(String op){
         String[] result = ss.split(" ");
         return result;
     }
+
 }
